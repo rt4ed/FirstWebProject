2513c087a6121c78fa4cd2eecb4ec40be4414a12
rt4ed <rt4ed2@gmail.com>
2021-06-30 00:54:49 +0400

    message

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const tabsBtn = document.querySelectorAll(".tabs__nav-btn");
 const tabsItems = document.querySelectorAll(".section-tv");
 const tabBut = document.querySelectorAll(".button-auth");
 const authContent = document.querySelectorAll(".modal")
+const tabButAuth = document.querySelectorAll(".button");
+const authHeader = document.querySelectorAll(".auth_header")
 
 tabsBtn.forEach(function (item){
     item.addEventListener("click", function() {
@@ -50,3 +52,39 @@ window.onclick =function (e){
         }
     };
 };
+
+document.getElementById('check').onclick=function (){
+    let login = document.getElementById('login').value;
+    let password = document.getElementById('password').value;
+    let AuthLog = 'ProkazZ';
+    let AuthPass = '1234';
+
+    if (login == AuthLog && password == AuthPass){
+        let modals=document.querySelectorAll("*[data-modal-window]")
+        for (let i=0; i<modals.length;i++){
+            modals[i].classList.remove('active');
+        }
+
+        tabButAuth.forEach(function (item){
+            item.classList.remove('active');
+        });
+
+        authHeader.forEach(function (item){
+            item.classList.add('active');
+        });
+
+        document.getElementById('auth_name').innerHTML = login;
+
+    }
+    else alert('Incorrect login or pass');
+}
+
+document.getElementById('auth_exitId').onclick=function (){
+    authHeader.forEach(function (item){
+        item.classList.remove('active');
+    });
+
+    tabButAuth.forEach(function (item){
+        item.classList.add('active');
+    });
+}
